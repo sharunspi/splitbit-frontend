04bd80023b52e4ded26abec2c94845e4d2868cfd
sharun k k <sharunkk1997@gmail.com>
2019-03-12 21:41:14 +0530

    added some files

---
+++ splitbit-frontend/src/components/Contents/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import React, { Component } from 'react';
+
+
+class Content extends Component {
+  render() 
+ 
+  {
+    return (
+      <div>
+         
+               <h4 className="he-1"> Project</h4>
+              <button className="bt-1"> Create new </button>
+      
+      </div>
+    );
+  }
+}
+
+export default Content;
--- splitbit-frontend/src/index.js
+++ splitbit-frontend/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import Head from './components/Head'
+import Content from './components/Contents/App';
+import Head from './components/head/Head'
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(<Head />, document.getElementById('head'));
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Content />, document.getElementById('content'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
